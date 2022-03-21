680b426204150b372fb5784c974affffaa892d6e
darmel <dario.martin.8a@gmail.com>
2022-03-21 01:10:23 -0300

    add some tests

--- main.py
+++ main.py
@@ -5,34 +5,39 @@ t = ''
 
 
 def enter_new_word(word):
-    l = len(word)
+    length = len(word)
     englishWords = []
     #print(f"entered word: {word} \nwith {l} letters")
     words = []
     #print(type(words))
-    for x in range(1, l + 1):
+    for x in range(1, length + 1):
         words += (list(permutations(word, x)))  # tuples
+        #print('dentro del for', words)
     words = (dict.fromkeys(words))  # trasnform to dictionary to delete duplicate
+    #print('en dictionario', words)
     words = list(words.keys())  # back to list
+    #print('back to list', words)
+    #print(words, length)
     for y in words:
         word = (''.join(y))
         #print(word, Dictionary.Dictionary.isEnglishWord(word))  # join lists
         if Dictionary.Dictionary.isEnglishWord(word):
             englishWords.append(word)
         #print(type(word)) #son string
-    return englishWords
+    return englishWords, len(englishWords), len(words)
 
 
-def main():
+def main(w=''):
     print("enter any word or 'exit' for exit")
     w = str.lower(input())
     if w == 'exit':
         return w
-    elif w == '':
-        return ''
+    #elif w == '':
+    #    return ''
     elif w.isalpha():
         print("are english words:")
-        print(', '.join(enter_new_word(w)))
+        english_words, cant_english_words, all_words = enter_new_word(w)
+        print(', '.join(english_words), cant_english_words, all_words)
         return w
     else:
         print(f'{w} is not a valid word')
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,25 @@
 
 from main import enter_new_word
+from main import main
+
 import pytest
 
 
+def test_empty_string(monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda: '')
+    assert (main('')) == 'not valid'
+
+
 def test_valid_word():
-    len(enter_new_word('casa')) > 0
+    assert 0 < (enter_new_word('casa')[1])
+
+
+def test_amount_of_permutations():
+    assert 64 == (enter_new_word('abcd')[2])
+
+
+def test_amount_english_words():
+    assert 9 == (enter_new_word('working')[1])
 
 
 def test_test():

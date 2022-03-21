70a3254b4f4bec7c84fd56f1cba35f8da02872f3
darmel <dario.martin.8a@gmail.com>
2022-03-21 16:01:05 -0300

    fix dictionary path for linux ands windows

--- Dictionary.py
+++ Dictionary.py
@@ -1,12 +1,15 @@
 import pathlib
-pathlib.Path()
+import os
+import sys
+
 
 class Dictionary:
     @staticmethod
     def isEnglishWord(wor):
         path = pathlib.Path("resources", "dictionary")
+        PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+        path = os.path.join(PROJECT_ROOT,path)
         dictionary = open(path, 'r')
-        #dictionary = open(str(pathlib.Path(__file__).absolute()) + f'\\..\\resources\\dictionary', 'r')
         dictionary_list = list((dictionary.read()).split('\n'))
         if wor in dictionary_list:
             return True

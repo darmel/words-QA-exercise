e84c911075e96efc44d0fc7d38059a53e95a2567
darmel <dario.martin.8a@gmail.com>
2022-03-21 12:16:20 -0300

    update dictionary path

--- Dictionary.py
+++ Dictionary.py
@@ -1,7 +1,12 @@
+import pathlib
+pathlib.Path()
+
 class Dictionary:
     @staticmethod
     def isEnglishWord(wor):
-        dictionary = open('C:\\Users\\Darmel\\PycharmProjects\\words-QA-exercise\\resources\\dictionary', 'r')
+        path = pathlib.Path("resources", "dictionary")
+        dictionary = open(path, 'r')
+        #dictionary = open(str(pathlib.Path(__file__).absolute()) + f'\\..\\resources\\dictionary', 'r')
         dictionary_list = list((dictionary.read()).split('\n'))
         if wor in dictionary_list:
             return True
